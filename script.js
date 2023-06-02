92a53578dde9a28fff17b941b629b5e2e183c97b
jackwang7003 <jackwang7003@gmail.com>
2023-06-01 22:32:47 -0500

    get one by ID completed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerContainer = document.getElementById("all-players-container");
 const newPlayerFormContainer = document.getElementById("new-player-form");
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = "YOUR COHORT NAME HERE";
+const cohortName = "2302-acc-ct-web-pt-b";
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
@@ -19,6 +19,10 @@ const fetchAllPlayers = async () => {
 
 const fetchSinglePlayer = async (playerId) => {
   try {
+    const response = await fetch(`${APIURL}/players/${id}`);
+    const result = await response.json();
+    console.log(result);
+    return result;
   } catch (err) {
     console.error(`Oh no, trouble fetching player #${playerId}!`, err);
   }
